9264571c896c6a277a3f877b5bbe70a19b2d792d
cheli5 <chynytapia@hotmail.com>
2024-09-26 16:32:18 -0700

    tarea de supermercado

---
+++ supermercado/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace supermercado
+{
+    //ID
+    //Nombre
+    //Precio
+    //Cantidad
+    class Articulo
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public float Precio { get; set; }
+        public int Cantidad { get; set; }
+    }
+}
